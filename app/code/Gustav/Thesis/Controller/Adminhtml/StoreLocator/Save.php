d714d28d678ea23e8c9a414b8fc453684e81c226
Vaimo <vaimo@Vaimos-MacBook-Pro.local>
2024-01-17 14:30:38 +0100

    Added validation logic for save new store

--- app/code/Gustav/Thesis/Controller/Adminhtml/StoreLocator/Save.php
+++ app/code/Gustav/Thesis/Controller/Adminhtml/StoreLocator/Save.php
@@ -39,23 +39,62 @@ public function execute()
             return $redirect->setPath('*/*/index');
         }
 
-        try {
-            $storeId = $data['store_id'] ?? null;
-            $storeLocator = $this->storeLocatorFactory->create();
 
-            if ($storeId) {
-                $this->storeLocatorResource->load($storeLocator, $storeId);
+        $validationErrors = $this->validateData($data);
+
+        if (!empty($validationErrors)) {
+            foreach ($validationErrors as $error) {
+                $this->messageManager->addErrorMessage($error);
             }
+            return $redirect->setPath('*/*/newstore');
+        }
 
+        try {
+            $storeLocator = $this->storeLocatorFactory->create();
             $storeLocator->setData($data);
             $this->storeLocatorResource->save($storeLocator);
 
-            $this->messageManager->addSuccessMessage(__('Store has been successfully saved.'));
-            return $redirect->setPath('*/*/index');
+            $this->messageManager->addSuccessMessage(__('The new store has been successfully saved.'));
+            return $redirect->setPath('*/*/edit', ['store_id' => $storeLocator->getId()]);
         } catch (\Exception $e) {
-            $this->messageManager->addErrorMessage(__('Error occurred while saving store: %1', $e->getMessage()));
+            $this->messageManager->addErrorMessage(__('Error occurred while saving the store: %1', $e->getMessage()));
             return $redirect->setPath('*/*/newstore');
         }
+    }
+
+    private function validateData($data): array
+    {
+        $errors = [];
+
+        if (empty($data['store_name'])) {
+            $errors[] = __('Store name is required.');
+        }
+
+        if (empty($data['address'])) {
+            $errors[] = __('Address is required.');
+        }
+
+         if (!preg_match("/^[a-zA-Z0-9, ]*$/", $data['city'])) {
+             $errors[] = __('City contains invalid characters.');
+         }
+
+        if (!empty($data['phone']) && !preg_match('/^\+?\d+(-\d+)*$/', $data['phone'])) {
+            $errors[] = __('Phone number format is invalid.');
+        }
+
+        if (!empty($data['postcode']) && !preg_match('/^\d{5}(-\d{4})?$/', $data['postcode'])) {
+            $errors[] = __('Postcode format is invalid.');
+        }
+
+        if ((!is_numeric($data['latitude'])) || $data['latitude'] < -90 || $data['latitude'] > 90) {
+            $errors[] = __('Latitude must be a number between -90 and 90.');
+        }
+
+        if ((!is_numeric($data['longitude'])) || $data['longitude'] < -180 || $data['longitude'] > 180) {
+            $errors[] = __('Longitude must be a number between -180 and 180.');
+        }
+
+        return $errors;
     }
 }
 
